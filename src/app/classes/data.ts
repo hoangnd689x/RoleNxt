72480c7f4ce22f6b8d73f4624bf6a2e27ae2e6e5
Ha Vinh Phu <havinhphu188@gmail.com>
2020-03-25 18:04:27 +0700

    Mocked data sample. for engineering

--- src/app/classes/data.ts
+++ src/app/classes/data.ts
@@ -4,78 +4,157 @@ import { Promotion } from './promotion';
 
 
 export const clusters: ClusterNode[] = [
-    {
-      id: 'c1',
-      label: 'Level 2',
-      childNodeIds: ['3', '4','5']
-    },
-    {
-        id: 'c2',
-        label: 'Level 3',
-        childNodeIds: ['1']
-    },
-    {
-        id: 'c3',
-        label: 'Level 1',
-        childNodeIds: ['2']
-    }
+    // {
+    //   id: 'c1',
+    //   label: 'Level 2',
+    //   childNodeIds: ['3', '4','5']
+    // },
+    // {
+    //     id: 'c2',
+    //     label: 'Level 3',
+    //     childNodeIds: ['1']
+    // },
+    // {
+    //     id: 'c3',
+    //     label: 'Level 1',
+    //     childNodeIds: ['2']
+    // }
   ];
 
   
 export const positions: Position[] = [
     {
         position_id: '1',
-        position_name: 'Financial Manager',
-        position_summary: 'Financial managers are responsible for the financial health of an organization. They produce financial reports, direct investment activities, and develop strategies and plans for the long-term financial goals of their organization. Financial managers work in many places, including banks and insurance companies.'
+        position_name: 'Member Engineering',
+        position_summary: 'Implement task with supervision'
     }, {
         position_id: '2',
-        position_name: 'Brokerage clerks',
-        position_summary: 'Brokerage clerks work for securities or brokerage firms. They are responsible for preparing and maintaining the records of financial transactions involving stocks, bonds, and other types of investments. Purchase-and-sale clerks match orders to buy with orders to sell and balance and verify stock trades.'
+        position_name: 'Technical Lead',
+        position_summary: 'Technical Lead is responsible review team member code'
     }, {
         position_id: '3',
-        position_name: 'Stock Broker',
-        position_summary: 'A stockbroker is a professional who executes buy and sell orders for stocks and other securities on behalf of clients. A stockbroker may also be known as a registered'
+        position_name: 'Architect',
+        position_summary: 'Architect: Role detail'
     }, {
         position_id: '4',
-        position_name: 'Financial Advisor',
-        position_summary: "A financial advisor is a professional that provides expertise for clients' decisions around money matters, personal finances, and investments. Financial advisors may work as an independent agent, or may be employed by a larger financial firm. Advisors must be licensed to carry out business with clients."
+        position_name: 'Principal Technologist',
+        position_summary: "Principal Technologist: Role Detail"
     },
     {
         position_id: '5',
-        position_name: 'Financial Analyst',
-        position_summary: 'Financial analysts work in banks, pension funds, insurance companies, and other businesses. Financial analysts provide guidance to businesses and individuals making investment decisions. They assess the performance of stocks, bonds, and other types of investments'
+        position_name: 'Principle consultant',
+        position_summary: 'Principle consultant: Role Detail'
+    },
+    {
+        position_id: '6',
+        position_name: 'Program Manager',
+        position_summary: 'Program Manager: Role Detail'
+    },
+    {
+        position_id: '7',
+        position_name: 'Engineering Manager',
+        position_summary: 'Engineering Manager: Role Detail'
+    },
+    {
+        position_id: '8',
+        position_name: 'DQA',
+        position_summary: 'DQA'
+    },
+    {
+        position_id: '9',
+        position_name: 'SQM',
+        position_summary: 'SQM'
+    },
+    {
+        position_id: '10',
+        position_name: 'SW Delivery Manager',
+        position_summary: 'SW Delivery Manager'
+    },
+    {
+        position_id: '11',
+        position_name: 'Product Manager',
+        position_summary: 'Product Manager'
+    },
+    {
+        position_id: '12',
+        position_name: 'Team Lead',
+        position_summary: 'Team Lead'
     }
   ];
 
   export const promotions: Promotion[] = [
     {
         promotion_id: '1',
-        start_position_id: '2',
-        next_position_id: '4'
+        start_position_id: '1',
+        next_position_id: '2'
     },
     {
         promotion_id: '2',
-        start_position_id: '2',
+        start_position_id: '1',
         next_position_id: '3'
     },
     {
         promotion_id: '3',
-        start_position_id: '2',
-        next_position_id: '5'
+        start_position_id: '1',
+        next_position_id: '4'
     },
     {
         promotion_id: '4',
-        start_position_id: '4',
-        next_position_id: '1'
+        start_position_id: '1',
+        next_position_id: '5'
     },
     {
         promotion_id: '5',
-        start_position_id: '3',
-        next_position_id: '1'
+        start_position_id: '2',
+        next_position_id: '9'
     },
     {
         promotion_id: '6',
-        start_position_id: '5',
-        next_position_id: '1'
+        start_position_id: '2',
+        next_position_id: '10'
     },
+    {
+        promotion_id: '7',
+        start_position_id: '2',
+        next_position_id: '11'
+    },
+    {
+        promotion_id: '8',
+        start_position_id: '3',
+        next_position_id: '2'
+    },
+    {
+        promotion_id: '9',
+        start_position_id: '3',
+        next_position_id: '8'
+    },
+    {
+        promotion_id: '10',
+        start_position_id: '4',
+        next_position_id: '5'
+    },
+    {//check id
+        promotion_id: '11',
+        start_position_id: '5',
+        next_position_id: '6'
+    },
+    {
+        promotion_id: '12',
+        start_position_id: '6',
+        next_position_id: '7'
+    },
+     {
+        promotion_id: '13',
+        start_position_id: '8',
+        next_position_id: '9'
+    },
+     {
+        promotion_id: '14',
+        start_position_id: '10',
+        next_position_id: '11'
+    }, {
+        promotion_id: '15',
+        start_position_id: '11',
+        next_position_id: '12'
+    }
   ];
